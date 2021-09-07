46e50d7078904af47617e443a42836d48979996b
Akshit Patel <akspa2803@gmail.com>
2021-09-07 23:15:34 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import './App.css';
+import PathfindingVisualizer from './PathfindingVisualizer/PathfindingVisualizer';
+
+function App() {
+  return (
+    <div>
+      <PathfindingVisualizer></PathfindingVisualizer>
+    </div>
+  )
+}
+
+export default App;
